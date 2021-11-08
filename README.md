8f6ad34aa588191a320dfce968792708062d98ab
Max <maksmogi@gmail.com>
2021-11-08 21:20:27 +0100

    Basic fun fuct app

---
+++ README.md
@@ -0,0 +1,5 @@
+# RandomFact
+
+Random fact in polish without using API. 
+
+Live: https://hero1230.github.io/RandomFact/
---
+++ js/main.js
@@ -0,0 +1,16 @@
+const currentDay = document.querySelector(".current-day");
+const funFact = document.querySelector(".fun-fact");
+
+const URL = "https://uselessfacts.jsph.pl/random.json?language=en";
+
+const day = new Date();
+currentDay.textContent = day.toLocaleString("pl", { weekday: "long" });
+
+const showRandomFact = () => {
+	fetch(URL)
+		.then((res) => res.json())
+		.then((data) => (funFact.textContent = data.text))
+		.catch((err) => console.log(err));
+};
+
+showRandomFact();
